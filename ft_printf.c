2b008b63ec8730a414a15d129fafa69d70efbb63
Nathaniel Zacharias <nzachari@c2r1s11.42singapore.sg>
2023-09-27 16:08:41 +0800

    completed set_specs, not tested

--- ft_printf.c
+++ ft_printf.c
@@ -27,18 +27,87 @@ static size_t	n_strlen(const char *str)
 	return (i);
 }
 
+static int	n_isdigit(char c)
+{
+	return (c >= '0' && c <= '9')
+}
+
+static int	validconversion(char c)
+{
+	return (c == '-' || c == '0' || c == '.' || c == '*' || c == '#' || 
+		c == ' ' || c == '+');
+}
+
+static int	validspecifier()
+{
+	return ();
+}
+
+static void	set_precision(va_list ap, t_specs *specs, const char *s, int i)
+{}
+
+static void	set_width(va_list ap, t_specs *specs)
+{}
+
+static int	set_specs(const char *s, int i, va_list ap, t_specs *specs)
+{
+	while (s[++i] && (validconversion(s[i]) || validspecifier(s[i]) || 
+		n_isdigit(s[i])))
+	{
+		if (s[i] == '+')
+			specs->plus = 1;
+		if (s[i] == '#')
+			specs->hash = 1;
+		if (s[i] == ' ')
+			specs->space = 1;
+		if (s[i] == '.')
+			set_precision(ap, specs, s, i);
+		if (s[i] == '*')
+			set_width(ap, specs);
+		if (s[i] == '0' && !(specs->left) && !(flags->width))
+			flags->zero = 1;
+		if (s[i] == '-')
+		{
+			specs->justifyleft = 1;
+			specs->zeroes = 0; //test this, can this be removed?
+		}
+		if (n_isdigit(s[i]) && specs->extraparams == 1)
+			specs->fieldwidth = 0;
+		else if (n_isdigit(s[i]))
+			specs.fieldwidth = (specs.fieldwidth * 10) + (c - '0');
+		if (validconversion(str[i]))
+		{
+			specs->specifier = str[i];
+			break ;
+		}
+	}
+	return (i);
+}
+
+
 int	ft_printf(const char *format, ...)
 {
 	int		i;
+	int		count;
 	va_list	ap;
+	t_specs	*specs;
 
 	if (!(*format) || !format)
 		return (0);
-	i = 0;
+	i = -1;
+	count = 0;
 	va_start(ap, format);
-	//parse
-	va_end(ap);
-	return (i);
+	while (format[++i] && format[i] == '%' && format[i + 1])
+	{
+		i = set_specs(format, i, ap, specs);
+		if (specs->specifier && validconversion(format[i]))
+			count += print_menu(format[i], ap, specs);
+		count += print_char(format[i], specs);
+	}
+	while (format[i])
+		count += write(1, &(format[i], 1);
+	va_end(ap); //is this optional?
+	return (count);
 }
 
 
